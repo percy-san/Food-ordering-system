56a3214c83e99be5799047c55d26f5dc32a840dc
Buhlebethu <percymkhonta@gmail.com>
2022-10-24 18:15:35 +0200

    Checkout added

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+//TAB FUNCTIONALITY FOR THE HOME PAGE MENU SELECTION
+const openitem = (evt, cityName) => {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
