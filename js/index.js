67e48672fcab654d8672963797f9e6b7d320fdac
Buhlebethu <percymkhonta@gmail.com>
2022-10-30 02:03:10 +0200

    dashboard added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 
-//TAB FUNCTIONALITY FOR THE HOME PAGE MENU SELECTION
-const openitem = (evt, cityName) => {
+//FORM VALIDATION//
+const validateSignIn = () =>{
+  
+}
+
+//TAB FUNCTIONALITY//
+const openItem = (evt, tabItem) => {
   let i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -10,7 +15,32 @@ const openitem = (evt, cityName) => {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(tabItem).style.display = "block";
   evt.currentTarget.className += " active";
 }
+document.getElementById("defaultOpen").click();
 
+const openSummary = (evt, tabItem) => {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("order-tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("order-tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabItem).style.display = "block";
+  evt.currentTarget.className += " active-order-summary";
+}
+document.getElementById("default").click();
+
+//ADMIN DASHBOARD JS/
+
+//toggle side bar
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector(".sidebar");
+
+btn.onclick = function(){
+    sidebar.classList.toggle("active");
+}
