d3eab649aff7516d60e0d4544058ad9faf78ae76
Buhlebethu <percymkhonta@gmail.com>
2022-10-31 02:09:59 +0200

    Added tables to view customers and orders, and finished off the dashboard section

--- js/index.js
+++ js/index.js
@@ -20,6 +20,19 @@ const openItem = (evt, tabItem) => {
 }
 document.getElementById("defaultOpen").click();
 
+
+
+//ADMIN DASHBOARD JS/
+
+//toggle side bar
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector(".sidebar");
+
+btn.onclick = function(){
+    sidebar.classList.toggle("active");
+}
+
+//ytabs
 const openSummary = (evt, tabItem) => {
   let i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("order-tabcontent");
@@ -31,16 +44,53 @@ const openSummary = (evt, tabItem) => {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
   document.getElementById(tabItem).style.display = "block";
-  evt.currentTarget.className += " active-order-summary";
+  evt.currentTarget.className += " active";
 }
 document.getElementById("default").click();
 
-//ADMIN DASHBOARD JS/
-
-//toggle side bar
-let btn = document.querySelector("#btn");
-let sidebar = document.querySelector(".sidebar");
-
-btn.onclick = function(){
-    sidebar.classList.toggle("active");
+//Tables javascript
+const searchOrdersTable = () => {
+  let input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("OrdersTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
 }
+
+const searchCustomersTable = () => {
+  let input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("searchInputCustomers");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("CustomerTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+
+
+
+
+
+
+
+
